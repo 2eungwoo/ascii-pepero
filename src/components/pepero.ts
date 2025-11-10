a0a655e15b8e0c08f856a2399af24249270ac30f
swlee <swo98@ajou.ac.kr>
2025-11-10 16:32:59 +0900

    fix: 코드 포맷팅 적용(탭 길이)

--- src/components/pepero.ts
+++ src/components/pepero.ts
@@ -3,17 +3,17 @@ import { ANIMALS } from "../models/animal_face";
 import { stripAnsi } from "../utils/strip-ansi";
 
 
-  // 뺴뺴로 막대를 입 위치로 배치
-  // left: 입 x좌표까지 공백
-  // right: col_width에서 content.length 뺸 만큼 공백
-  function alignAndMakeCell(content: string) {
+// 뺴뺴로 막대를 입 위치로 배치
+// left: 입 x좌표까지 공백
+// right: col_width에서 content.length 뺸 만큼 공백
+function alignAndMakeCell(content: string) {
     const visible = stripAnsi(content) // 색상 ansi 코드 제거
     const left = " ".repeat(MOUTH_X);
     const right = " ".repeat(COL_W - MOUTH_X - visible.length);
     return left + content + right;
-  }
+}
 
-  function makePeperoPart(content: string, repeat: number) {
+function makePeperoPart(content: string, repeat: number) {
     const cell = alignAndMakeCell(content);
     for (let i = 0; i < repeat; i++) {
       let row = "";
@@ -21,22 +21,22 @@ import { stripAnsi } from "../utils/strip-ansi";
         row += cell;
       }
       console.log(row);
-    }
   }
+}
 
-  export function renderPepero(chocoLen:number, flavorColor:string) {
-      // 초코부분
-      makePeperoPart(flavorColor + PEPERO_BAR + RESET_COLOR, chocoLen);
+export function renderPepero(chocoLen:number, flavorColor:string) {
+    // 초코부분
+    makePeperoPart(flavorColor + PEPERO_BAR + RESET_COLOR, chocoLen);
 
-      // 과자부분
-      makePeperoPart(STICK_PART_COLOR + PEPERO_BAR + RESET_COLOR, STICK_PART_LEN)
+    // 과자부분
+    makePeperoPart(STICK_PART_COLOR + PEPERO_BAR + RESET_COLOR, STICK_PART_LEN)
 
-      // tip부분 (고정)
-      makePeperoPart(PEPERO_GAP_BLANK, 1);
+    // tip부분 (고정)
+    makePeperoPart(PEPERO_GAP_BLANK, 1);
 
-      // blank
-      const blank_part = CHOCO_PART_MAX - chocoLen;
-      makePeperoPart(PEPERO_GAP_BLANK, blank_part);
+    // blank
+    const blank_part = CHOCO_PART_MAX - chocoLen;
+    makePeperoPart(PEPERO_GAP_BLANK, blank_part);
 
-      console.log("");
-  }
+    console.log("");
+}
--- src/utils/terminal.ts
+++ src/utils/terminal.ts
@@ -2,24 +2,24 @@
 
 export function clearScreen() {
     process.stdout.write("\u001b[H\u001b[2J"); // 커서 좌상단, 콘솔 전체 지우기
-  }
+}
 
-  export function moveCursorUp(lines: number) {
+export function moveCursorUp(lines: number) {
     process.stdout.write(`\u001B[${lines}A`); // 커서 n줄 위로
     // 빼빼로 줄어들때마다 동물이랑 과자높이 고정시키면서 새로 clear -> render 하려면
     // 계속 바뀌는 line수만큼 커서 올려서 같은 자리에 새 프레임으로 덮어써줘야됨
-  }
+}
 
-  export function padRight(faceAscii: string, colWidth: number) {
+export function padRight(faceAscii: string, colWidth: number) {
     const n = colWidth - faceAscii.length;
     return faceAscii + (n > 0 ? " ".repeat(n) : ""); // 간격 폭 공백만큼 채워서 고정시킴 요소있으면 그걸로 채움
-  }
+}
 
-  export function center(animalName: string, colWidth: number) {
+export function center(animalName: string, colWidth: number) {
     const n = colWidth - animalName.length;
     const L = Math.floor(n / 2);
     const R = n - L;
     return " ".repeat(Math.max(0, L)) + animalName + " ".repeat(Math.max(0, R));
     // 가운데 정렬 용
     // L/R 공백 분리해서 맞춤
-  }
+}
